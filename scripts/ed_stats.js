cc18a254ccde8a19fe9b8ba676d7deb77a2a0cb9
girvain <gavinross88@hotmail.co.uk>
2018-11-15 01:10:37 +0000

    made functions out of code

--- scripts/ed_stats.js
+++ scripts/ed_stats.js
@@ -20,18 +20,6 @@ $(document).ready(function () {
             success: function (data) {
                 console.log(data);
 
-                var output = '<ul class="searchresults">';
-                // XML approach to output ul elements of the graph data                
-                $(data).find('wb\\:data').find("wb\\:data").each(function (key, val) {
-                    var desc = '<li>' + $(val).find('wb\\:indicator').text() + '</li>';
-                    var date = '<li>' + $(val).find('wb\\:date').text() + '</li>';
-                    var value = '<li>' + $(val).find('wb\\:value').text() + '</li>';
-
-                    output += desc + date + value + '</ul>';
-                    $('#stats-area').append(output);
-                    output = '<ul class="searchresults">';
-                });
-
                 // make JSON objects with the data from the XML 
                 var descArray = [];
                 var dateArray = [];
@@ -41,48 +29,58 @@ $(document).ready(function () {
                     descArray.push($(val).find('wb\\:indicator').text());
                     dateArray.push($(val).find('wb\\:date').text());
                     // the xml of the % is a string so needs converted to an int
-                    var intVal = parseInt($(val).find('wb\\:value').text());
+                    var intVal = parseFloat($(val).find('wb\\:value').text());
                     valueArray.push(intVal);
                 });
 
                 console.log(dateArray, valueArray);
 
-                var chartObj = {
-                    "type": "bar",
-                    "title": {
-                        "text": "Chart Data Object"
-                    },
-                    "series": [{
-                        "values": valueArray
-                    }]
-                };
-
-                console.log(chartObj);
-                zingchart.render({ // Render Method[3]
-                    id: 'male-female-chart',
-                    data: chartObj,
-                    height: 400,
-                    width: '100%'
-                });
+                var obj = makeObj(dateArray, valueArray);
+                renderGraph('male-female-chart', obj);
             }
 
         }); // end of ajax
     });
-
-
 });
 
-function makeObj(array) {
+/* Function to create an object for graphing */
+function makeObj(dates, values) {
     var chartObj = {
         "type": "bar",
         "title": {
             "text": "Chart Data Object"
         },
+        "scale-x": {
+            "values": dates
+        },
         "series": [{
-            "values": array
+            "values": values
         }]
     };
-        
-    //chartObj.series.values = array;
     return chartObj;
+}
+
+//  Function to render a graph. Args: id or class
+function renderGraph(id, chartObj) {
+    zingchart.render({ // Render Method[3]
+        id: id,
+        data: chartObj,
+        height: 400,
+        width: '100%'
+    });
+
+}
+
+// XML approach to output ul elements of the graph data                
+function outputXMLList() {
+    var output = '<ul class="searchresults">';
+    $(data).find('wb\\:data').find("wb\\:data").each(function (key, val) {
+        var desc = '<li>' + $(val).find('wb\\:indicator').text() + '</li>';
+        var date = '<li>' + $(val).find('wb\\:date').text() + '</li>';
+        var value = '<li>' + $(val).find('wb\\:value').text() + '</li>';
+
+        output += desc + date + value + '</ul>';
+        $('#stats-area').append(output);
+        output = '<ul class="searchresults">';
+    });
 }
